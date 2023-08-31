1d58be34c8d22bcfa29d87d3b3c850237c9172f7
Hirak Roy <royhirakp@gmail.com>
2023-08-31 21:35:48 +0530

    <h1>app path = &apos;/home&apos;</h1>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import { Box } from "@mui/material";
-import Header from "@/components/Header";
-import Products from "@/components/Products/Products";
+
 export default function Home() {
   return (
     <Box>
-      <h1>app path = '/home'</h1>
+      <h1>app path = &apos;/home&apos;</h1>
     </Box>
   );
 }
