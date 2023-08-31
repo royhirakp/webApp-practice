1c2baa8baf6e75bc5c09ac2eaff37d3543c50d48
Hirak Roy <royhirakp@gmail.com>
2023-08-31 21:26:38 +0530

    cc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,8 @@ import Header from "@/components/Header";
 import Products from "@/components/Products/Products";
 export default function Home() {
   return (
-    <Box
-    // minHeight="100%"
-    // sx={
-    //   {
-    //     // boxSizing: "border-box",
-    //     // padding: "20px",
-    //     // margin: 3,
-    //     // padding: {
-    //     //   xs: 1,
-    //     //   sm: 2,
-    //     //   md: 3,
-    //     //   lg: 4,
-    //     //   xl: 5,
-    //     // },
-    //   }
-    // }
-    >
-      {/* <Header />
-      <Products /> */}
+    <Box>
+      <h1>app path = '/home'</h1>
     </Box>
   );
 }
